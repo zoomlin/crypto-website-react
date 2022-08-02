3c0ec3c12ddf91c2efec076601c9feb274a67762
Денис Владимирович <zoomlin@gmail.com>
2022-08-02 18:52:35 +0300

    project ready commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Hero from './components/Hero';
+import Header from './components/Header';
+import NavMobile from './components/NavMobile';
+import Stats from './components/Stats';
+import Why from './components/Why';
+import Calculate from './components/Calculate';
+import Trade from './components/Trade';
+import   Features from './components/Features';
+import  Newsletter from './components/Newsletter';
+import  Footer from './components/Footer';
+
+import Aos from 'aos';
+import 'aos/dist/aos.css'
+const App = () => {
+  const [navMobile, setNavMobile] = React.useState(false);
+  React.useEffect(() => {
+    Aos.init({
+      duration: 2500,
+      delay: 400,
+    })
+  }, []);
+  return <div className='overflow-hidden'>
+    <Header setNavMobile={setNavMobile}/>
+    <Hero/>
+    <div className={`${navMobile ? 'right-0': '-right-full'} fixed z-10 top-0 h-full transition-all duration-200`}>
+      <NavMobile setNavMobile={setNavMobile}/>
+    </div>
+    <Stats />
+    <Why />
+    <Calculate />
+    <Trade />
+    <Features />
+    <Newsletter />
+    <Footer/>
+  </div>;
+};
+
+export default App;
---
+++ src/components/Calculate.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import CalcForm from './CalcForm';
+
+const Calculate = () => {
+  return (
+    <section className="section">
+      <div className="container mx-auto">
+        <div className="text-center">
+          <h2
+            className="section-title"
+            data-aos="fade-up"
+            data-aos-offset="400"
+          >
+            Check how much you can earn
+          </h2>
+          <p
+            className="section-subtitle text-lg mb-16 max-w-[622px] mx-auto"
+            data-aos="fade-up"
+            data-aos-offset="450"
+          >
+            Let's check yours hash rate to see how much you will earn today.
+          </p>
+        </div>
+        <CalcForm />
+      </div>
+    </section>
+  );
+};
+
+export default Calculate;
